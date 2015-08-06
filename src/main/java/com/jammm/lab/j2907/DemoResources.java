a036829fe21b52c0ce074cd17b511fe43b9c5eba
Greg Wiley <gwiley@cj.com>
2015-08-06 09:55:36 -0700

    parent resource gets path

--- src/main/java/com/jammm/lab/j2907/DemoResources.java
+++ src/main/java/com/jammm/lab/j2907/DemoResources.java
@@ -1,16 +1,17 @@
 package com.jammm.lab.j2907;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 @Produces("text/plain")
 @Consumes("text/plain")
+@Path("/")
 public class DemoResources {
 
-	@GET
-	@Path("/echo")
+	@POST
+	@Path("echo")
 	public String echo(String what) {
 		return what;
 	}
