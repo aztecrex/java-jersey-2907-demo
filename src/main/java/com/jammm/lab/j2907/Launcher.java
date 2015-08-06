b3173b9c3433c9e65ec69cced0c65acf87145612
Greg Wiley <gwiley@cj.com>
2015-08-06 10:20:00 -0700

    logging

--- src/main/java/com/jammm/lab/j2907/Launcher.java
+++ src/main/java/com/jammm/lab/j2907/Launcher.java
@@ -1,5 +1,7 @@
 package com.jammm.lab.j2907;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
@@ -7,14 +9,23 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class Launcher {
 
+	static {
+
+		try (InputStream logConfigIs = Launcher.class.getResourceAsStream("logger.properties")) {
+			java.util.logging.LogManager.getLogManager().readConfiguration(logConfigIs);
+		} catch (final IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("cannot configure logging", e);
+		}
+
+	}
+
 	public static void main(String[] args) throws Exception {
 
 		final ResourceConfig app = new ResourceConfig().register(DemoResources.class)
 				.register(RequirePrincipalFilter.class);
 
 		GrizzlyHttpServerFactory.createHttpServer(URI.create("http://localhost:8081/"), app, true);
-
-		// Thread.currentThread().join();
 
 	}
 
